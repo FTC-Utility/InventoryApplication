66bbdb3d0499bac1b547102ad0ba1a5f1e63867a
Eyuel Taddese <eyuel.taddese4@gmail.com>
2017-01-04 17:12:16 -0500

    UserToken changed

--- src/main/java/com/ftc/fia/domain/UserToken.java
+++ src/main/java/com/ftc/fia/domain/UserToken.java
@@ -1,6 +1,9 @@
 package com.ftc.fia.domain;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * Created by Eyuel Tadesse on 12/30/2016.
@@ -9,24 +12,24 @@ import javax.persistence.*;
 @Table(name = "user_token")
 public class UserToken {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private int id;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name ="toke_type", referencedColumnName = "id")
     private TokeType tokenType;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User userId;
 
-    @Column(name = "value")
     private String value;
+
+    @Column(name = "expire_date")
+    @Type(type = "com.ftc.fia.util.LocalDateTimeAttributeConverter")
+    private LocalDateTime expiredDate;
 
     public UserToken() {
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public int getId() {
         return id;
     }
@@ -35,6 +38,8 @@ public class UserToken {
         this.id = id;
     }
 
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name ="toke_type_id", referencedColumnName = "id")
     public TokeType getTokenType() {
         return tokenType;
     }
@@ -43,11 +48,30 @@ public class UserToken {
         this.tokenType = tokenType;
     }
 
+    @Column(name = "value")
     public String getValue() {
         return value;
     }
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(LocalDateTime expiredDate) {
+        this.expiredDate = expiredDate;
     }
 }
