94d0a948b6fef6c971f5a1731ebdf17d16df5931
Franklin Perez <FPerez@ftc-llc.com>
2017-01-09 16:05:34 -0500

    Sub-Task FIA-135 (Create 2 Factor Authentication Processing) for User Story FIA-64 (Create New Password Entry): Removed unnecesary "public" from "findByUser" method.

--- src/main/java/com/ftc/fia/repository/IUserPasswordRepository.java
+++ src/main/java/com/ftc/fia/repository/IUserPasswordRepository.java
@@ -14,5 +14,5 @@ import java.util.List;
 @Repository
 public interface IUserPasswordRepository extends JpaRepository<UserPassword, Integer> {
 
-    public List<UserPassword> findByUser(User user);
+   List<UserPassword> findByUser(User user);
 }
